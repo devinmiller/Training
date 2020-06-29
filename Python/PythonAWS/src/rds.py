1e4c0be683564083f0576c316f731830d125b506
Devin Miller <devinmiller@codeonthebrain.com>
2020-06-29 11:28:08 -0700

    EC2 CLI Improvements

---
+++ Python/PythonAWS/src/rds.py
@@ -0,0 +1,9 @@
+import argparse
+import boto3
+
+rds = boto3.resource('rds', region_name='us-west-2')
+
+def configure_arguments(resource_parser):
+    parser = resource_parser.add_parser('rds', help='Manage RDS instances')
+
+    subparser = parser.add_subparsers(description='RDS Commands', dest='rds')
