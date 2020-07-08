4db91cc97737e6d6f16d625165d7be2a33521e3f
Devin Miller <millede@fsafood.net>
2020-07-08 08:54:57 -0700

    Forgot a few changes.

--- Python/PythonAWS/src/resources/rds.py
+++ Python/PythonAWS/src/resources/rds.py
@@ -19,6 +19,8 @@ class RDS(Client):
         cmd_create = parser.add_parser('create', help='Create RDS instance')
         cmd_create.add_argument('engine', help='The name of the database engine to be used')
         cmd_create.add_argument('--dbname', help='The name of the database created with the instance')
+        cmd_create.add_argument('--dbiid', required=true, help='The DB instance identifier')
+        cmd_create.add_argument('--dbicls', default='db.t3', help='The DB instance class')
 
     def _build_list_command(self, parser):
         cmd_list = parser.add_parser('list', help='List RDS instances')
